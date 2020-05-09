c7057ca8caeae2ae3f45c3c8ef105259c5c84d5d
Ben Soutendijk <devaent1316@gmail.com>
2020-05-09 18:47:36 -0500

    Sign Up Page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from '../Header';
-import LogIn from '../../pages/LogIn';
+import SignUp from '../../pages/SignUp';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <LogIn />
+      <SignUp />
     </div>
   );
 }
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SignUp.scss';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+function SignUp() {
+  return (
+    <div className="LogIn">
+      <Jumbotron className="bg-dark text-light">
+        <Form onSubmit={(e: React.FormEvent<HTMLFormElement>) => e.preventDefault()}>
+          <Form.Group>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Email" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" placeholder="Confirm Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Sign Up
+          </Button>
+        </Form>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default SignUp;
