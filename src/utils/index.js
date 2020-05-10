428f7889541e1658b77bdde47c0c7f2325313ca6
Ben Soutendijk <devaent1316@gmail.com>
2020-05-09 20:23:16 -0500

    fix import

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,12 +2,10 @@ import debounce from './debounce';
 import getUniqueValues from './getUniqueValues';
 import groupBy from './groupBy';
 import mapToKey from './mapToKey';
-import createQueryString from './createQueryString';
 
 export {
     debounce,
     getUniqueValues,
     groupBy,
     mapToKey,
-    createQueryString,
 };
