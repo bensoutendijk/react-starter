68375c4b1f866e511cac1508d6d3683fb6a14803
Ben Soutendijk <devaent1316@gmail.com>
2020-05-09 20:23:32 -0500

    initialize store

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
